3c5dd5a79c10561f605b5b6d4afd12f4a987240a
Siskat <yinmin.zarhayda@gmail.com>
2018-08-27 18:49:15 +1000

    skeleton

--- src/App.js
+++ src/App.js
@@ -3,16 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      values: {},
+      standard: ''
+    }
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    let formValues = this.state.values;
+    let name = event.target.name;
+    let value = event.target.value;
+
+    formValues[name] = value;
+
+    this.setState({
+      formValues
+    });
+    this.calculate();
+  }
+
+  calculate() {
+    let sd = this.state.values.amount/1000*this.state.values.percent*0.789;
+    this.setState({standard:sd});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="App">
+        <div id="calculation">
+          <h1 class="padding">Calculate your standard drinks here!</h1>
+          <input class="calc" name="amount" placeholder="In millilitres" value={this.state.values["amount"]}  onChange={this.handleChange.bind(this)}/>
+          <br/>
+          <input class="calc" name="percent" placeholder="eg. 50 for 50%" value={this.state.values["percent"]}  onChange={this.handleChange.bind(this)}/>
+          <h2 class="padding">Standard drinks</h2><p>{this.state.standard}</p>
+        </div>
       </div>
     );
   }
